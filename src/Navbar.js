f4bc536b43c4bc0845b3f7107183aa1c40dbd547
Jegalas <sadigkhudaverdiyev@gmail.com>
2024-01-02 18:12:58 +0400

    update//1

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,14 +8,12 @@ const NavbarComponent = () => {
       <Navbar variant="light" bg="light">
         <Nav className="me-auto d-flex justify-content-around w-100 align-items-center">
           <NavLink to="/">Home</NavLink>
-          <NavLink to="/movies">Movie</NavLink>
+          <NavLink to="/movies">Flash Cards</NavLink>
           <NavLink to="/contact">Contact</NavLink>
           <NavDropdown title="Github" id="basic-nav-dropdown">
-            <NavDropdown.Item href="https://github.com/Shafiga2000">Shafiga Hasanova</NavDropdown.Item>
-            <NavDropdown.Item href="https://github.com/ulviyyamammadova">Ulviyya Mammdova</NavDropdown.Item>
-            <NavDropdown.Item href="https://github.com/isalahov12393">Isa Salahov</NavDropdown.Item>
+            <NavDropdown.Item href="https://github.com/Jegalas">Profile</NavDropdown.Item>
+            <NavDropdown.Item href="https://github.com/Jegalas/webmobile_03">Repository</NavDropdown.Item>
           </NavDropdown>
-          <NavLink to="#">Youtube</NavLink>
         </Nav>
       </Navbar>
     </header>
